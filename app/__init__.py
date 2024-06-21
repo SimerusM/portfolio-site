0cc28d766f5d67625d2b09368038d86c65dd0e13
Simerus Mahesh <63352151+SimerusM@users.noreply.github.com>
2024-06-20 20:59:59 -0400

    Created dynamic jinja template and added a hobbies page

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,21 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+# List of pages, update this list for dynamic page adding
+pages = [
+    {'name': 'Home', 'endpoint': 'index'},
+    {'name': 'Hobbies', 'endpoint': 'hobbies'}
+]
 
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def hobbies():
+    hobbies_list = ['Reading', 'Hiking', 'Programming', 'Photography']
+    return render_template('multi_items.html', title="Hobbies", items=hobbies_list)
+
+@app.context_processor
+def inject_pages():
+    return dict(pages=pages)
