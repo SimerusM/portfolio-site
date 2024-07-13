420b0f61ee0dfc37a3b0404d2ba0650828a83293
Simerus Mahesh <63352151+SimerusM@users.noreply.github.com>
2024-07-13 14:50:31 -0400

    Added boot service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,9 +6,5 @@ tmux kill-server
 # Ensure the latest changes from the main branch are pulled from GitHub
 git fetch && git reset origin/main --hard
 
-# Activate the Python virtual environment and install requirements
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-
-# Start a new detached tmux session and run the Flask server
-tmux new-session -d -s current_session "source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
+# Restart boot service
+sudo systemctl restart myportfolio.service
