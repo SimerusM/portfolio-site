bc54548f08b0197510e84a2b7c22af08563d1a1f
Simerus Mahesh <63352151+SimerusM@users.noreply.github.com>
2024-08-12 17:23:42 -0400

    test

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -7,4 +7,4 @@ cd /root/portfolio-site
 git fetch && git reset origin/main --hard
 
 docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml up --build -d
+docker compose -f docker-compose.prod.yml up -d --build
